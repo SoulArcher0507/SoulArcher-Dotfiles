922c1278172650dbadd66fac564296578c05d544
SoulArcher0507 <enea.ares2004@gmail.com>
2025-07-01 18:02:04 +0200

    wallpaper with image

--- Soul's theme/swaybg/wallpaper.sh
+++ Soul's theme/swaybg/wallpaper.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
 
 WALLPAPER_DIR="${HOME}/Pictures/Wallpapers"
-WALLPAPER_PATH=$(find $HOME/Pictures/Wallpapers/ -maxdepth 1 -type f -exec basename {} \; | rofi -dmenu -p "Enter wallpaper name:")
-PAPER="${WALLPAPER_DIR}/${WALLPAPER_PATH}"
+FIFO="/tmp/rofi-preview.fifo"
 
-if [ -z $WALLPAPER_PATH ]; then
-    exit 1
-fi
+# Prepara la named pipe
+[[ -e $FIFO ]] && rm $FIFO
+mkfifo $FIFO
+
+# Demone ueberzug: ascolta sulla pipe e mostra l’immagine corrispondente
+bash -c '
+  exec 3<> '"$FIFO"'
+  while read -u 3 line; do
+    # rimuove eventiali preview precedenti
+    ueberzug layer --parser bash <<-EOF
+    [{"action":"remove","identifier":"preview"}]
+EOF
+    # se c’è un file valido, aggiunge il preview
+    if [[ -f "'"$WALLPAPER_DIR"'/$line" ]]; then
+      ueberzug layer --parser bash <<-EOF
+      [{"action":"add",
+        "identifier":"preview",
+        "x":700, "y":200,
+        "width":400, "height":300,
+        "path":"'"$WALLPAPER_DIR"'/$line"}]
+EOF
+    fi
+  done
+' &
+
+# Costruisci la lista e lancia rofi
+mapfile -t files < <(find "$WALLPAPER_DIR" -maxdepth 1 -type f -printf '%f\n')
+selection=$(printf '%s\n' "${files[@]}" \
+  | rofi -dmenu -p "Scegli sfondo:" \
+         -mesg "Muovi su/giu per cambiare preview" \
+         -input-reading $FIFO)
+
+# Pulisce e termina ueberzug
+kill $!       # uccide il demone in background
+rm $FIFO
+
+# Selezione valida?
+[[ -z $selection ]] && exit 1
+
+PAPER="$WALLPAPER_DIR/$selection"
 
 cp $PAPER "${WALLPAPER_DIR}/active/active.jpg"
 
