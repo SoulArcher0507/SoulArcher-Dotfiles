b08e5e3157e82fdd70eed1c4ab931a16fae9a37c
SoulArcher0507 <enea.ares2004@gmail.com>
2025-07-02 10:39:52 +0200

    wlogout

--- Soul's theme/sway/scripts/wlogout.sh
+++ Soul's theme/sway/scripts/wlogout.sh
@@ -9,7 +9,7 @@ EOF
 w_margin=$(( res_h * 27 / scale ))
 
 # Lancia wlogout con i bordi calcolati
-# wlogout -b 5
-wlogout -b 5 -R "$w_margin" -L "$w_margin"
+wlogout -b 5
+# wlogout -b 5 -R "$w_margin" -L "$w_margin"
 # wlogout -b 5 -T "$w_margin" -B "$w_margin"
 # wlogout -b 5 -m "$w_margin"
