a507ca2650cb165b7bbe75952c4b01e8c96ce989
SoulArcher0507 <enea.ares2004@gmail.com>
2025-09-04 18:23:18 +0200

    push

---
+++ Quickshell-PC/swaybg/wallpaper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+WALLPAPER_DIR="${HOME}/Pictures/Wallpapers"
+WALLPAPER_PATH=$(ls -p $WALLPAPER_DIR | grep -v '/$' | while read A ; do  echo -en "$A\x00icon\x1f$WALLPAPER_DIR/$A\n"; done | rofi -dmenu -p "Enter wallpaper name:")
+PAPER="${WALLPAPER_DIR}/${WALLPAPER_PATH}"
+
+
+
+if [ -z $WALLPAPER_PATH ]; then
+    exit 1
+fi
+
+cp $PAPER "${WALLPAPER_DIR}/active/active.jpg"
+
+pkill -x swaybg
+swaybg -i \"$PAPER\" -m fill &
+
+magick "$WALLPAPER_DIR/active/active.jpg" -resize 75% "$WALLPAPER_DIR/active/active_blur.jpg"
+magick "$WALLPAPER_DIR/active/active_blur.jpg" -blur "50x30" "$WALLPAPER_DIR/active/active_blur.jpg"
+magick "$WALLPAPER_DIR/active/active.jpg" -gravity Center -extent 1:1 "$WALLPAPER_DIR/active/active_square.jpg"
+
+$HOME/.config/hypr/scripts/reload.sh &
+
+$HOME/.config/wal/colors.sh "$PAPER"
+
+
---
+++ Quickshell-PC/wal/colors.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# === Config ===
+#WALLPAPER="$HOME/Pictures/background.png"
+WALLPAPER=$1
+WAYBAR_CSS="$HOME/.config/waybar/colors.css"
+SWAYNC_CSS="$HOME/.config/swaync/colors.css"
+WLOGOUT_CSS="$HOME/.config/wlogout/colors.css"
+ROFI_RASI="$HOME/.config/rofi/colors.rasi"
+HYPR="$HOME/.config/hypr/colors.conf"
+
+
+# === Generate pywal colors without applying them ===
+# To avoid changing the terminal's colors: wal -n -s -t -i "$WALLPAPER"
+wal -i "$WALLPAPER"
+
+# === Read colors from pywal ===
+WAL_COLORS="$HOME/.cache/wal/colors.json"
+if [ ! -f "$WAL_COLORS" ]; then
+    echo "Error: $WAL_COLORS not found."
+    exit 1
+fi
+
+#Waybar + SwayNC
+cp $HOME/.cache/wal/colors-waybar.css $WAYBAR_CSS
+cp $HOME/.cache/wal/colors-waybar.css $SWAYNC_CSS
+cp $HOME/.cache/wal/colors-waybar.css $WLOGOUT_CSS
+
+background=$(jq -r '.special.background' "$WAL_COLORS")
+foreground=$(jq -r '.special.foreground' "$WAL_COLORS")
+
+#Rofi
+{
+    echo "* {"
+    echo "bg: $background;"
+    echo "fg: $foreground;"
+
+    for i in {0..15}; do
+        color=$(jq -r ".colors.color$i" "$WAL_COLORS")
+        echo "color$i: $color;"
+    done
+    echo "}"
+} > "$ROFI_RASI"
+
+#Hyprland
+{
+    echo "\$bg = rgb(${background//#/})"
+    echo "\$fg = rgb(${foreground//#/})"
+
+    for i in {0..15}; do
+        color=$(jq -r ".colors.color$i" "$WAL_COLORS" | sed 's/#//')
+        echo "\$color$i = rgb($color)"
+    done
+} > "$HYPR"
+
+# percorso del JSON di Pywal
+WAL_COLORS="$HOME/.cache/wal/colors.json"
+
+# prendo il valore esadecimale di color4 (es. "#aabbcc")
+color4=$(jq -r '.colors.color4' "$WAL_COLORS")
+
+$HOME/.config/waybar/scripts/svg-color-switcher-openai.sh $color4
+$HOME/.config/waybar/scripts/svg-color-switcher-obsidian.sh $color4
+
+$HOME/.config/hypr/scripts/reload.sh
